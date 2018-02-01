9f7f64a9bf5c838192a77d9592f2cfc385e8e9f4
Nikhil Thakur <nikhilthakur3197@gmail.com>
2018-02-01 19:43:17 +0530

    ..

--- js/history.js
+++ js/history.js
@@ -1,7 +1,7 @@
 app.controller('fellaController', function($scope, $http) {
-    $http.get("http://54.156.18.72/feedbacker/gettransaction")
-    .then(function(response) {
-        $scope.transactions = response.data;
-        console.log($scope.transactions);
-    });
+    // $http.get("http://54.156.18.72/feedbacker/gettransaction")
+    // .then(function(response) {
+    //     $scope.transactions = response.data;
+    //     console.log($scope.transactions);
+    // });
 });
